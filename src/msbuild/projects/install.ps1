0a6435b1a950ad5c0040a5d66ad261bffd7501c3
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-06-27 16:24:51 +1200

    Adding the install.ps1 and uninstall.ps1 which will be used to add the imports to the project file.
    #23

---
+++ src/msbuild/projects/install.ps1
@@ -0,0 +1,38 @@
+param($installPath, $toolsPath, $package, $project)
+
+# calculate the relative path between the project and the props file
+$projectUri = New-Object System.Uri -ArgumentList $project.FullName
+Write-Host "project file is at: $projectUri" 
+
+$propsFileUri = New-Object System.Uri -ArgumentList (Join-Path (Join-Path $installPath 'build') '${TargetsFile}.props')
+Write-Host "properties file is at: $propsFileUri" 
+
+$relativeFilePath = $projectUri.MakeRelativeUri($propsFileUri).ToString()
+Write-Host "Relative path from projects file to props file is: $relativeFilePath" 
+
+# Need to load MSBuild assembly if it's not loaded yet.
+Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+
+# Grab the loaded MSBuild project for the project
+$msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
+
+# create the Import node
+$importElement = $msbuild.Xml.CreateImportElement($relativeFilePath)
+
+$itemGroupNode = $msbuild.Xml.ItemGroups | Select-Object -First 1
+Write-Host ("Found first item group at: " + $itemGroupNode.ToString())
+
+$msbuild.Xml.InsertBeforeChild($importElement, $itemGroupNode)
+Write-Host ("Inserting before the first item group: " + $importElement.ToString())
+
+# Calculate the relative path between the project and the targets file
+$targetsFileUri = New-Object System.Uri -ArgumentList (Join-Path (Join-Path $installPath 'build') '${TargetsFile}.targets')
+Write-Host "properties file is at: $targetsFileUri" 
+
+$relativeFilePath = $projectUri.MakeRelativeUri($targetsFileUri).ToString()
+Write-Host "Relative path from projects file to targets file is: $relativeFilePath" 
+
+$importElement = $msbuild.Xml.CreateImportElement($relativeFilePath)
+$msbuild.Xml.AppendChild($importElement)
+
+$project.Save()
