12a33d912070cd079270e343391c0c256b13fbf8
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-04-27 17:26:38 +1200

    Adding the C# test project that will be used to test the nBuildKit standard scripts and the nBuildKit C# scripts
    references #7

---
+++ tests/msbuild/csharp/src/nBuildKit.Test.CSharp.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using NBuildKit.Test.CSharp.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBuildKit.Test.CSharp.Console
+{
+    /// <summary>
+    /// The entry point for the application.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// The entry method for the application.
+        /// </summary>
+        static void Main()
+        {
+            var helloWorld = new HelloWorld();
+            System.Console.WriteLine(helloWorld.SayHello());
+        }
+    }
+}
---
+++ tests/msbuild/csharp/src/nBuildKit.Test.CSharp.Library/HelloWorld.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBuildKit.Test.CSharp.Library
+{
+    /// <summary>
+    /// Provides methods to say hello to the world.
+    /// </summary>
+    public class HelloWorld
+    {
+        private readonly string _name = AssemblyName();
+        private readonly string _version = AssemblyVersion();
+
+        /// <summary>
+        /// Says hello to the world.
+        /// </summary>
+        /// <returns>A string containing the message.</returns>
+        public string SayHello()
+        {
+            return string.Format(
+                CultureInfo.InvariantCulture,
+                "Hello world from: {0} [{1}]",
+                _name,
+                _version);
+        }
+
+        private static string AssemblyName()
+        {
+            var attribute = (AssemblyTitleAttribute)Assembly.GetExecutingAssembly().GetCustomAttribute(typeof(AssemblyTitleAttribute));
+            return attribute.Title;
+        }
+
+        private static string AssemblyVersion()
+        {
+            var attribute = (AssemblyInformationalVersionAttribute)Assembly.GetExecutingAssembly().GetCustomAttribute(typeof(AssemblyInformationalVersionAttribute));
+            return attribute.InformationalVersion;
+        }
+    }
+}
