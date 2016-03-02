aa17d459d7c6ab32c4b23e850d6c30cebbbe2cf9
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-03-02 18:18:39 +1300

    Simplifying the installation of the project nuget packages

--- src/msbuild/projects/install.ps1
+++ src/msbuild/projects/install.ps1
@@ -1,54 +1,51 @@
 param($installPath, $toolsPath, $package, $project)
 
-$filesToInsert = @( ${TargetsFiles} )
-
-# Need to load MSBuild assembly if it"s not loaded yet.
-Add-Type -AssemblyName "Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
-
 # Grab the loaded MSBuild project for the project
 $msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
 
-foreach($file in $filesToInsert)
+# calculate the relative path between the project and the props file
+$projectUri = New-Object System.Uri -ArgumentList $project.FullName
+Write-Host "project file is at: $projectUri"
+
+$file = "import"
+
+Write-Host "Inserting import.props file ... "
+$propsFilePath = Join-Path (Join-Path $installPath "build") "$file.props"
+if (Test-Path $propsFilePath)
 {
-    # calculate the relative path between the project and the props file
-    $projectUri = New-Object System.Uri -ArgumentList $project.FullName
-    Write-Host "project file is at: $projectUri"
+    $propsFileUri = New-Object System.Uri -ArgumentList $propsFilePath
+    Write-Host "properties file is at: $propsFileUri"
 
-    $propsFilePath = Join-Path (Join-Path $installPath "build") "$file.props"
-    if (Test-Path $propsFilePath)
-    {
-        $propsFileUri = New-Object System.Uri -ArgumentList $propsFilePath
-        Write-Host "properties file is at: $propsFileUri"
+    $relativeFilePath = $projectUri.MakeRelativeUri($propsFileUri).ToString()
+    Write-Host "Relative path from projects file to props file is: $relativeFilePath"
 
-        $relativeFilePath = $projectUri.MakeRelativeUri($propsFileUri).ToString()
-        Write-Host "Relative path from projects file to props file is: $relativeFilePath"
+    # create the Import node
+    $importElement = $msbuild.Xml.CreateImportElement($relativeFilePath)
+    $importElement.Condition = " Exists('$relativeFilePath') "
 
-        # create the Import node
-        $importElement = $msbuild.Xml.CreateImportElement($relativeFilePath)
-        $importElement.Condition = " Exists('$relativeFilePath') "
+    $itemGroupNode = $msbuild.Xml.ItemGroups | Select-Object -First 1
+    Write-Host ("Found first item group")
 
-        $itemGroupNode = $msbuild.Xml.ItemGroups | Select-Object -First 1
-        Write-Host ("Found first item group at: " + $itemGroupNode.ToString())
+    $msbuild.Xml.InsertBeforeChild($importElement, $itemGroupNode)
+    Write-Host ("Inserted insert.props before the first item group")
+}
 
-        $msbuild.Xml.InsertBeforeChild($importElement, $itemGroupNode)
-        Write-Host ("Inserting before the first item group: " + $importElement.ToString())
-    }
+# Calculate the relative path between the project and the targets file
+Write-Host "Inserting import.targets file ... "
+$targetsFilePath = Join-Path (Join-Path $installPath "build") "$file.targets"
+if (Test-Path $targetsFilePath)
+{
+    $targetsFileUri = New-Object System.Uri -ArgumentList $targetsFilePath
+    Write-Host "Targets file is at: $targetsFileUri"
 
-    # Calculate the relative path between the project and the targets file
-    $targetsFilePath = Join-Path (Join-Path $installPath "build") "$file.targets"
-    if (Test-Path $targetsFilePath)
-    {
-        $targetsFileUri = New-Object System.Uri -ArgumentList $targetsFilePath
-        Write-Host "Targets file is at: $targetsFileUri"
+    $relativeFilePath = $projectUri.MakeRelativeUri($targetsFileUri).ToString()
+    Write-Host "Relative path from projects file to targets file is: $relativeFilePath"
 
-        $relativeFilePath = $projectUri.MakeRelativeUri($targetsFileUri).ToString()
-        Write-Host "Relative path from projects file to targets file is: $relativeFilePath"
+    $importElement = $msbuild.Xml.CreateImportElement($relativeFilePath)
+    $importElement.Condition = " Exists('$relativeFilePath') "
 
-        $importElement = $msbuild.Xml.CreateImportElement($relativeFilePath)
-        $importElement.Condition = " Exists('$relativeFilePath') "
-
-        $msbuild.Xml.AppendChild($importElement)
-    }
+    $msbuild.Xml.AppendChild($importElement);
+    Write-Host ("Inserted insert.targets at the end of the project file.")
 }
 
 $project.Save()
--- src/msbuild/projects/uninstall.ps1
+++ src/msbuild/projects/uninstall.ps1
@@ -1,31 +1,25 @@
 param($installPath, $toolsPath, $package, $project)
 
-# Need to load MSBuild assembly if it’s not loaded yet.
-Add-Type -AssemblyName "Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
-
 # Grab the loaded MSBuild project for the project
 $msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
 
-$filesToRemove = @( ${TargetsFiles} )
-foreach($file in $filesToRemove)
+$file = "import"
+$propertyImport = $msbuild.Xml.Imports | Where-Object { $_.Project.Endswith("$file.props") }
+
+# Remove the import and save the project
+if ($propertyImport -ne $null)
 {
-    $propertyImport = $msbuild.Xml.Imports | Where-Object { $_.Project.Endswith("$file.props") }
+    $msbuild.Xml.RemoveChild($propertyImport) | out-null
+    Write-Host "Removed $file.props"
+}
 
-    # Remove the import and save the project
-    if ($propertyImport -ne $null)
-    {
-        $msbuild.Xml.RemoveChild($propertyImport) | out-null
-        Write-Host "Removed $file.props"
-    }
+$targetImport = $msbuild.Xml.Imports | Where-Object { $_.Project.Endswith("$file.targets") }
 
-    $targetImport = $msbuild.Xml.Imports | Where-Object { $_.Project.Endswith("$file.targets") }
-
-    # Remove the import and save the project
-    if ($targetImport -ne $null)
-    {
-        $msbuild.Xml.RemoveChild($targetImport) | out-null
-        Write-Host "Removed $file.targets"
-    }
+# Remove the import and save the project
+if ($targetImport -ne $null)
+{
+    $msbuild.Xml.RemoveChild($targetImport) | out-null
+    Write-Host "Removed $file.targets"
 }
 
 #save the changes.
