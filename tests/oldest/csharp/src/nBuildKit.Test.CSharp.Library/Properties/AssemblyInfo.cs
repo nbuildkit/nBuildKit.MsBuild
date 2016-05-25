0973d5b0a6ab7b27dd7bf8f98143f8072dd2ea9a
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-05-26 11:24:41 +1200

    Adding the C# test files for the oldest supported version of nBuildkit
    references #140

---
+++ tests/oldest/csharp/src/nBuildKit.Test.CSharp.Library/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("nBuildKit.Test.CSharp.Library")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("Not_A_Configuration")]
+[assembly: AssemblyCompany("Not_A_Company")]
+[assembly: AssemblyCopyright("Not_A_Copyright")]
+[assembly: AssemblyProduct("nBuildKit.Test.CSharp.Library")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f016b7a5-448d-4afc-8066-e3fb001cbc76")]
+
+// Indicate the assembly is CLS compliant.
+[assembly: CLSCompliant(true)]
+
+// Version numbers
+[assembly: AssemblyVersion("1.2.3.4")]
+[assembly: AssemblyFileVersion("5.6.7.8")]
+[assembly: AssemblyInformationalVersion("9.10.11-fake+12")]
