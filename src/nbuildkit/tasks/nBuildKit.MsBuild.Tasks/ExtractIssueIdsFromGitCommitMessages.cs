5baba5f9412d14e7f39a7d9f2c44d7e6ecc540fa
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-12-10 00:46:31 +1300

    Moving the ExtractIssueIdsFromGitCommitMessages task
    references #230

---
+++ src/nbuildkit/tasks/nBuildKit.MsBuild.Tasks/ExtractIssueIdsFromGitCommitMessages.cs
@@ -0,0 +1,106 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="nBuildKit">
+// Copyright (c) nBuildKit. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace NBuildKit.MsBuild.Tasks
+{
+    /// <summary>
+    /// Defines a <see cref="ITask"/> that extracts the issue numbers from all the commits that have not been merged to the
+    /// <see cref="MergeTargetBranch"/>. Issue IDs are parsed using the <see cref="IssueIdRegex"/> or the default regex
+    /// of '(?:#)(\d+)' if no regular expression is specified.
+    /// </summary>
+    public sealed class ExtractIssueIdsFromGitCommitMessages : GitCommandLineToolTask
+    {
+        // Grab any number that is preceded by a hash sign
+        private const string DefaultIssueIdRegex = @"(?:#)(\d+)";
+
+        /// <inheritdoc/>
+        public override bool Execute()
+        {
+            string[] unmergedCommits = UnmergedCommits();
+            Log.LogMessage(MessageImportance.Low, "Unmerged commits: ");
+            foreach (var commit in unmergedCommits)
+            {
+                Log.LogMessage(MessageImportance.Low, commit);
+            }
+
+            var list = new SortedList<string, ITaskItem>();
+            var regex = new System.Text.RegularExpressions.Regex(!string.IsNullOrEmpty(IssueIdRegex) ? IssueIdRegex : DefaultIssueIdRegex);
+            foreach (var commit in unmergedCommits)
+            {
+                var logMessage = InvokeGit(
+                    new[]
+                    {
+                        string.Format(
+                            "log -n 1 --pretty=format:%B {0}",
+                            commit)
+                    });
+                var issueIdMatch = regex.Match(logMessage);
+                if (issueIdMatch.Success)
+                {
+                    var issueId = issueIdMatch.Groups[1].Value;
+                    Log.LogMessage(MessageImportance.Low, "Issue for commit: [" + commit + "] is: [" + issueId + "]");
+                    if (!list.ContainsKey(issueId))
+                    {
+                        var newItem = new TaskItem(issueId);
+                        list.Add(issueId, newItem);
+                    }
+                }
+            }
+
+            IssueIds = list.Values.ToArray();
+
+            return !Log.HasLoggedErrors;
+        }
+
+        /// <summary>
+        /// Gets or sets the collection containing the issue IDs which were extracted from the set of unmerged commits.
+        /// </summary>
+        [Output]
+        public ITaskItem[] IssueIds
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the regular expression that is used to extract the issue ID from a GIT commit message. Defaults
+        /// to '(?:#)(\d+)', i.e. a hash sign followed by a number of digits.
+        /// </summary>
+        public string IssueIdRegex
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the branch to which the current set of commits should be merged.
+        /// </summary>
+        [Required]
+        public string MergeTargetBranch
+        {
+            get;
+            set;
+        }
+
+        private string[] UnmergedCommits()
+        {
+            // Get the SHA1 values for all the commits that haven't been merged to the target branch yet
+            var gitOutput = InvokeGit(
+                new[]
+                {
+                    string.Format("cherry {0}", MergeTargetBranch)
+                });
+            return gitOutput.Replace("+ ", Environment.NewLine).Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
---
+++ src/nbuildkit/tasks/nBuildKit.MsBuild.Tasks/GitCommandLineToolTask.cs
@@ -0,0 +1,85 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="nBuildKit">
+// Copyright (c) nBuildKit. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Microsoft.Build.Framework;
+
+namespace NBuildKit.MsBuild.Tasks
+{
+    /// <summary>
+    /// Defines the base class for <see cref="ITask"/> classes that work with the GIT command line tool.
+    /// </summary>
+    public abstract class GitCommandLineToolTask : CommandLineToolTask
+    {
+        /// <summary>
+        /// Gets or sets the path to the GIT command line executable.
+        /// </summary>
+        [Required]
+        public ITaskItem GitExecutablePath
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Invokes the GIT command line tool with the given arguments in the provided workspace directory.
+        /// </summary>
+        /// <param name="arguments">The collection of arguments.</param>
+        /// <returns>The output of the GIT process</returns>
+        protected string InvokeGit(IEnumerable<string> arguments)
+        {
+            var text = new StringBuilder();
+            DataReceivedEventHandler standardOutputHandler = (s, e) =>
+            {
+                if (!string.IsNullOrWhiteSpace(e.Data))
+                {
+                    text.Append(e.Data);
+                }
+            };
+
+            DataReceivedEventHandler standardErrorHandler = (s, e) =>
+            {
+                if (!string.IsNullOrWhiteSpace(e.Data))
+                {
+                    Log.LogWarning(e.Data);
+                }
+            };
+
+            var exitCode = InvokeCommandlineTool(
+                GitExecutablePath,
+                arguments,
+                Workspace,
+                standardOutputHandler: standardOutputHandler,
+                standardErrorHandler: standardErrorHandler);
+            if (exitCode != 0)
+            {
+                Log.LogError(
+                    string.Format(
+                        "{0} exited with a non-zero exit code. Exit code was: {1}",
+                        System.IO.Path.GetFileName(GitExecutablePath.ItemSpec),
+                        exitCode));
+                Log.LogError(string.Format("Output was: {0}", text));
+
+                return null;
+            }
+
+            return text.ToString();
+        }
+
+        /// <summary>
+        /// Gets or sets the full path to the workspace that contains the .git directory.
+        /// </summary>
+        [Required]
+        public ITaskItem Workspace
+        {
+            get;
+            set;
+        }
+    }
+}
