81c6f8c98a9c21a9865d4472816598a184720dd1
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-12-10 21:30:46 +1300

    Moving the GitCommit task
    references #230

---
+++ src/nbuildkit/tasks/nBuildKit.MsBuild.Tasks/GitCommit.cs
@@ -0,0 +1,44 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="nBuildKit">
+// Copyright (c) nBuildKit. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using Microsoft.Build.Framework;
+
+namespace NBuildKit.MsBuild.Tasks
+{
+    /// <summary>
+    /// Defines a <see cref="ITask"/> that performs a Git commit.
+    /// </summary>
+    public sealed class GitCommit : GitCommandLineToolTask
+    {
+        /// <inheritdoc/>
+        public override bool Execute()
+        {
+            var arguments = new List<string>();
+            {
+                arguments.Add("commit ");
+                arguments.Add("--all ");
+                arguments.Add(string.Format("--message=\"{0}\" ", Message.TrimEnd('\\')));
+                arguments.Add("--quiet ");
+            }
+
+            InvokeGit(arguments);
+
+            return !Log.HasLoggedErrors;
+        }
+
+        /// <summary>
+        /// Gets or sets the commit message.
+        /// </summary>
+        [Required]
+        public string Message
+        {
+            get;
+            set;
+        }
+    }
+}
