91c2593f595cc0cfa6b1aeeaa73379deeeecf1b7
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-12-11 22:22:34 +1300

    Moving the PublicKeySignatureFromKeyFile task
    references #230

---
+++ src/nbuildkit/tasks/nBuildKit.MsBuild.Tasks/PublicKeySignatureFromKeyFile.cs
@@ -0,0 +1,130 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="nBuildKit">
+// Copyright (c) nBuildKit. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Microsoft.Build.Framework;
+
+namespace NBuildKit.MsBuild.Tasks
+{
+    /// <summary>
+    /// Defines a <see cref="ITask"/> that extracts the public key from a key file.
+    /// </summary>
+    public sealed class PublicKeySignatureFromKeyFile : CommandLineToolTask
+    {
+        /// <inheritdoc/>
+        public override bool Execute()
+        {
+            var snExeFileName = Path.GetFileName(GetAbsolutePath(SnExe));
+
+            var publicKeyFile = Path.Combine(GetAbsolutePath(TemporaryDirectory), Path.GetRandomFileName());
+            try
+            {
+                {
+                    Log.LogMessage(MessageImportance.Normal, string.Format("Extracting public key file from {0} ...", Input));
+                    var arguments = new[] { string.Format("-p \"{0}\" \"{1}\"", Input, publicKeyFile.TrimEnd('\\')) };
+                    var exitCode = InvokeCommandlineTool(
+                        SnExe,
+                        arguments);
+                    if (exitCode != 0)
+                    {
+                        Log.LogError(
+                            string.Format(
+                                "{0} exited with a non-zero exit code while trying to extract the public key file from the complete key file. Exit code was: {1}",
+                                Path.GetFileName(snExeFileName),
+                                exitCode));
+                        return false;
+                    }
+                }
+
+                var text = new StringBuilder();
+                {
+                    Log.LogMessage(MessageImportance.Normal, "Extracting public key ...");
+                    var arguments = new[] { string.Format("-tp \"{0}\"", publicKeyFile.TrimEnd('\\')) };
+                    DataReceivedEventHandler standardOutputHandler = (s, e) =>
+                    {
+                        text.Append(e.Data);
+                        if (!string.IsNullOrWhiteSpace(e.Data))
+                        {
+                            Log.LogMessage(MessageImportance.Low, e.Data);
+                        }
+                    };
+                    var exitCode = InvokeCommandlineTool(
+                        SnExe,
+                        arguments,
+                        standardOutputHandler: standardOutputHandler);
+                    if (exitCode != 0)
+                    {
+                        Log.LogError(
+                            string.Format(
+                                "{0} exited with a non-zero exit code while trying to extract the public key from the public key file. Exit code was: {1}",
+                                Path.GetFileName(snExeFileName),
+                                exitCode));
+                        return false;
+                    }
+                }
+
+                const string startString = "Public key (hash algorithm: sha1):";
+                const string endString = "Public key token is";
+                var publicKeyText = text.ToString();
+                var startIndex = publicKeyText.IndexOf(startString);
+                var endIndex = publicKeyText.IndexOf(endString);
+                PublicKey = publicKeyText.Substring(startIndex + startString.Length, endIndex - (startIndex + startString.Length));
+            }
+            finally
+            {
+                if (File.Exists(publicKeyFile))
+                {
+                    File.Delete(publicKeyFile);
+                }
+            }
+
+            return !Log.HasLoggedErrors;
+        }
+
+        /// <summary>
+        /// Gets or sets the full path to the key file.
+        /// </summary>
+        [Required]
+        public ITaskItem Input
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the public key.
+        /// </summary>
+        [Output]
+        public string PublicKey
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the full path to the strong name tool (sn.exe).
+        /// </summary>
+        [Required]
+        public ITaskItem SnExe
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the full path to a directory into which temporary files may be created.
+        /// </summary>
+        [Required]
+        public ITaskItem TemporaryDirectory
+        {
+            get;
+            set;
+        }
+    }
+}
