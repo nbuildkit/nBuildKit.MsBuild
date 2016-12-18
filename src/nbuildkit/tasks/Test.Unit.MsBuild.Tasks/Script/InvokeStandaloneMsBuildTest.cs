a5f3b77a6f3c782a07d8be8dec3ff3318d9d5729
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-12-19 00:08:35 +1300

    Adding the test skeleton
    references #231

---
+++ src/nbuildkit/tasks/Test.Unit.MsBuild.Tasks/Script/InvokeStandaloneMsBuildTest.cs
@@ -0,0 +1,61 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="nBuildKit">
+// Copyright (c) nBuildKit. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NBuildKit.MsBuild.Tasks.Script
+{
+    [TestFixture]
+    [SuppressMessage(
+        "Microsoft.StyleCop.CSharp.DocumentationRules",
+        "SA1600:ElementsMustBeDocumented",
+        Justification = "Unit tests do not need documentation.")]
+    public sealed class InvokeStandaloneMsBuildTest : TaskTest
+    {
+        [Test]
+        public void ExecuteWithFailingProject()
+        { }
+
+        [Test]
+        public void ExecuteWithMissingProjects()
+        { }
+
+        [Test]
+        public void ExecuteWithMissingProjectsAndSkipNonexistantProjects()
+        { }
+
+        [Test]
+        public void ExecuteWithMultipleProjects()
+        { }
+
+        [Test]
+        public void ExecuteWithMultipleProjectsAndFailingProjects()
+        { }
+
+        [Test]
+        public void ExecuteWithMultipleProjectsAndFailingProjectsAndStopOnFirstFailure()
+        { }
+
+        [Test]
+        public void ExecuteWithProjectWithNoOutput()
+        { }
+
+        [Test]
+        public void ExecuteWithProjectWithOutput()
+        { }
+
+        [Test]
+        public void ExecuteWithSingleProject()
+        { }
+    }
+}
