8b01e5a22a6637dc136e31e42c5090346e59d040
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-02-25 21:50:38 +1300

    Adding script to copy files from the file system and from a nuget package
    references #255

---
+++ src/nbuildkit/tasks/Test.Unit.MsBuild.Tasks/FileSystem/SearchPackagesDirectoryForNuGetPackageTest.cs
@@ -0,0 +1,169 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="nBuildKit">
+// Copyright (c) nBuildKit. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.IO.Abstractions.TestingHelpers;
+using Microsoft.Build.Utilities;
+using NBuildKit.MsBuild.Tasks.Tests;
+using NUnit.Framework;
+
+namespace NBuildKit.MsBuild.Tasks.FileSystem
+{
+    [TestFixture]
+    [SuppressMessage(
+        "Microsoft.StyleCop.CSharp.DocumentationRules",
+        "SA1600:ElementsMustBeDocumented",
+        Justification = "Unit tests do not need documentation.")]
+    public sealed class SearchPackagesDirectoryForNuGetPackageTest : TaskTest
+    {
+        [Test]
+        public void ExecuteWithMultipleVersions()
+        {
+            InitializeBuildEngine();
+
+            var knownPackages = new[]
+            {
+                "A.B.1.0.0",
+                "A.B.1.1.0",
+                "A.B.1.1.1",
+                "A.C.1.0.0",
+                "D.E.1.0.0",
+            };
+
+            var packagesDirectory = "d:\\mock\\packages";
+            var fileSystem = new MockFileSystem();
+            {
+                fileSystem.AddDirectory(packagesDirectory);
+                foreach (var package in knownPackages)
+                {
+                    fileSystem.AddDirectory(Path.Combine(packagesDirectory, package));
+                }
+            }
+
+            var task = new SearchPackagesDirectoryForNuGetPackage(fileSystem);
+            task.BuildEngine = BuildEngine.Object;
+            task.PackagesDirectory = new TaskItem(packagesDirectory);
+            task.PackageToLocate = "A.B";
+
+            var result = task.Execute();
+            Assert.IsTrue(result);
+
+            var output = task.Path;
+            Assert.AreEqual(Path.Combine(packagesDirectory, knownPackages[2]), output.ItemSpec);
+        }
+
+        [Test]
+        public void ExecuteWithoutVersion()
+        {
+            InitializeBuildEngine();
+
+            var knownPackages = new[]
+            {
+                "A.B.1.0.0",
+                "A.B.1.1.0",
+                "A.B.1.1.1",
+                "A.C.1.0.0",
+                "D.E.1.0.0",
+            };
+
+            var packagesDirectory = "d:\\mock\\packages";
+            var fileSystem = new MockFileSystem();
+            {
+                fileSystem.AddDirectory(packagesDirectory);
+                foreach (var package in knownPackages)
+                {
+                    fileSystem.AddDirectory(Path.Combine(packagesDirectory, package));
+                }
+            }
+
+            var task = new SearchPackagesDirectoryForNuGetPackage(fileSystem);
+            task.BuildEngine = BuildEngine.Object;
+            task.PackagesDirectory = new TaskItem(packagesDirectory);
+            task.PackageToLocate = "F.G";
+
+            var result = task.Execute();
+            Assert.IsTrue(result);
+
+            var output = task.Path;
+            Assert.IsNull(output);
+        }
+
+        [Test]
+        public void ExecuteWithSingleVersion()
+        {
+            InitializeBuildEngine();
+
+            var knownPackages = new[]
+            {
+                "A.B.1.0.0",
+                "A.B.1.1.0",
+                "A.B.1.1.1",
+                "A.C.1.0.0",
+                "D.E.1.0.0",
+            };
+
+            var packagesDirectory = "d:\\mock\\packages";
+            var fileSystem = new MockFileSystem();
+            {
+                fileSystem.AddDirectory(packagesDirectory);
+                foreach (var package in knownPackages)
+                {
+                    fileSystem.AddDirectory(Path.Combine(packagesDirectory, package));
+                }
+            }
+
+            var task = new SearchPackagesDirectoryForNuGetPackage(fileSystem);
+            task.BuildEngine = BuildEngine.Object;
+            task.PackagesDirectory = new TaskItem(packagesDirectory);
+            task.PackageToLocate = "A.C";
+
+            var result = task.Execute();
+            Assert.IsTrue(result);
+
+            var output = task.Path;
+            Assert.AreEqual(Path.Combine(packagesDirectory, knownPackages[3]), output.ItemSpec);
+        }
+
+        [Test]
+        public void ExecuteWithSubPackages()
+        {
+            InitializeBuildEngine();
+
+            var knownPackages = new[]
+            {
+                "A.B.1.0.0",
+                "A.B.1.1.0",
+                "A.B.1.1.1",
+                "A.B.C.1.0.0",
+                "A.B.D.1.0.0",
+                "D.E.1.0.0",
+            };
+
+            var packagesDirectory = "d:\\mock\\packages";
+            var fileSystem = new MockFileSystem();
+            {
+                fileSystem.AddDirectory(packagesDirectory);
+                foreach (var package in knownPackages)
+                {
+                    fileSystem.AddDirectory(Path.Combine(packagesDirectory, package));
+                }
+            }
+
+            var task = new SearchPackagesDirectoryForNuGetPackage(fileSystem);
+            task.BuildEngine = BuildEngine.Object;
+            task.PackagesDirectory = new TaskItem(packagesDirectory);
+            task.PackageToLocate = "A.B";
+
+            var result = task.Execute();
+            Assert.IsTrue(result);
+
+            var output = task.Path;
+            Assert.AreEqual(Path.Combine(packagesDirectory, knownPackages[2]), output.ItemSpec);
+        }
+    }
+}
---
+++ src/nbuildkit/tasks/nBuildKit.MsBuild.Tasks/FileSystem/SearchPackagesDirectoryForNuGetPackage.cs
@@ -0,0 +1,134 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="nBuildKit">
+// Copyright (c) nBuildKit. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Globalization;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using NBuildKit.MsBuild.Tasks.Core;
+
+namespace NBuildKit.MsBuild.Tasks.FileSystem
+{
+    /// <summary>
+    /// Defines a <see cref="ITask"/> that searches a given directory for a specific NuGet package and returns the full path to the directory containing the package.
+    /// </summary>
+    public sealed class SearchPackagesDirectoryForNuGetPackage : BaseTask
+    {
+        private readonly IFileSystem _fileSystem;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SearchPackagesDirectoryForNuGetPackage"/> class.
+        /// </summary>
+        public SearchPackagesDirectoryForNuGetPackage()
+            : this(new System.IO.Abstractions.FileSystem())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SearchPackagesDirectoryForNuGetPackage"/> class.
+        /// </summary>
+        /// <param name="fileSystem">The object that provides access to the file system.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown if <paramref name="fileSystem"/> is <see langword="null" />.
+        /// </exception>
+        public SearchPackagesDirectoryForNuGetPackage(IFileSystem fileSystem)
+        {
+            if (fileSystem == null)
+            {
+                throw new ArgumentNullException("fileSystem");
+            }
+
+            _fileSystem = fileSystem;
+        }
+
+        /// <inheritdoc/>
+        public override bool Execute()
+        {
+            var packagesInfo = _fileSystem.DirectoryInfo.FromDirectoryName(GetAbsolutePath(PackagesDirectory));
+            var potentialPaths = packagesInfo.EnumerateDirectories(
+                    string.Format(
+                        CultureInfo.InvariantCulture,
+                        "{0}.*",
+                        PackageToLocate),
+                    SearchOption.TopDirectoryOnly);
+            Log.LogMessage(
+                MessageImportance.Low,
+                "Searching for {0} located the following potential directories: {1}",
+                PackageToLocate,
+                string.Join(", ", potentialPaths.Select(i => i.FullName)));
+
+            string selectedPath = null;
+            var selectedVersion = new Version();
+            foreach (var path in potentialPaths)
+            {
+                var versionText = path.Name.Substring(PackageToLocate.Length).Trim('.').Trim();
+
+                Version packageVersion;
+                if (!Version.TryParse(versionText, out packageVersion))
+                {
+                    Log.LogMessage(
+                        MessageImportance.Low,
+                        "Path {0} is not a match for package {1}",
+                        path.FullName,
+                        PackageToLocate);
+
+                    continue;
+                }
+
+                if (packageVersion > selectedVersion)
+                {
+                    Log.LogMessage(
+                        MessageImportance.Low,
+                        "Path {0} is a better match for package {1} than {2}",
+                        path.FullName,
+                        PackageToLocate,
+                        selectedPath);
+
+                    selectedVersion = packageVersion;
+                    selectedPath = path.FullName;
+                }
+            }
+
+            Path = (selectedPath != null) ? new TaskItem(selectedPath) : null;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the package that should be located.
+        /// </summary>
+        [Required]
+        public string PackageToLocate
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the full path to the packages directory.
+        /// </summary>
+        [Required]
+        public ITaskItem PackagesDirectory
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the full path to the directory containing the given tool.
+        /// </summary>
+        [Output]
+        public ITaskItem Path
+        {
+            get;
+            set;
+        }
+    }
+}
