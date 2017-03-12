3bf8ca1ccff4a8276cecb3cb24d7fc217435e4cf
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-03-12 22:33:02 +1300

    Rewriting some of the unit tests to use the mock items
    references #234

--- src/nbuildkit/tasks/Test.Unit.MsBuild.Tasks/Script/InvokePowershellCommandTest.cs
+++ src/nbuildkit/tasks/Test.Unit.MsBuild.Tasks/Script/InvokePowershellCommandTest.cs
@@ -5,8 +5,14 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
+using Moq;
+using NBuildKit.MsBuild.Tasks.Core;
 using NBuildKit.MsBuild.Tasks.Tests;
 using NUnit.Framework;
 
@@ -30,19 +36,67 @@ namespace NBuildKit.MsBuild.Tasks.Script
 
             InitializeBuildEngine();
 
+            var invokedPath = string.Empty;
+            var invokedArgs = new List<string>();
+            var invokedWorkingDirectory = string.Empty;
+            Action<StringDictionary> environmentVariableBuilder = null;
+            var invoker = new Mock<IApplicationInvoker>();
+            {
+                invoker.Setup(
+                    i => i.Invoke(
+                        It.IsAny<string>(),
+                        It.IsAny<IEnumerable<string>>(),
+                        It.IsAny<string>(),
+                        It.IsAny<Action<StringDictionary>>(),
+                        It.IsAny<DataReceivedEventHandler>(),
+                        It.IsAny<DataReceivedEventHandler>(),
+                        It.IsAny<bool>()))
+                    .Callback<string, IEnumerable<string>, string, Action<StringDictionary>, DataReceivedEventHandler, DataReceivedEventHandler, bool>(
+                        (path, args, dir, e, o, err, f) =>
+                        {
+                            invokedPath = path;
+                            invokedArgs.AddRange(args);
+                            invokedWorkingDirectory = dir;
+                            environmentVariableBuilder = e;
+                        });
+            }
+
             var task = new InvokePowershellCommand();
             task.BuildEngine = BuildEngine.Object;
             task.IgnoreErrors = false;
-
             task.Command = command;
-            var result = task.Execute();
 
+            var result = task.Execute();
             Assert.IsTrue(result);
 
             var output = task.Output;
             Assert.AreEqual(text, output);
 
-            VerifyNumberOfLogMessages(numberOfNormalMessages: 2);
+            Assert.AreEqual(@"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe", invokedPath);
+
+            Assert.AreEqual(5, invokedArgs.Count);
+            Assert.AreEqual("-NoLogo", invokedArgs[0]);
+            Assert.AreEqual("-NonInteractive", invokedArgs[1]);
+            Assert.AreEqual("-NoProfile", invokedArgs[2]);
+            Assert.AreEqual("-ExecutionPolicy Bypass", invokedArgs[3]);
+            Assert.AreEqual("-WindowStyle Hidden", invokedArgs[4]);
+            Assert.AreEqual(
+                string.Format(
+                    CultureInfo.InvariantCulture,
+                    "-Command \"{0}\"",
+                    command),
+                invokedArgs[5]);
+
+            invoker.Verify(
+                i => i.Invoke(
+                    It.IsAny<string>(),
+                    It.IsAny<IEnumerable<string>>(),
+                    It.IsAny<string>(),
+                    It.IsAny<Action<StringDictionary>>(),
+                    It.IsAny<DataReceivedEventHandler>(),
+                    It.IsAny<DataReceivedEventHandler>(),
+                    It.IsAny<bool>()),
+                Times.Once());
         }
 
         [Test]
@@ -52,17 +106,43 @@ namespace NBuildKit.MsBuild.Tasks.Script
             var errorText = "It is all wrong";
             var command = string.Format(
                 CultureInfo.InvariantCulture,
-                "$ErrorActionPreference = 'Continue'; Write-Error '{0}'; Write-Output '{1}'",
+                "$ErrorActionPreference = 'Stop'; Write-Error '{0}'; Write-Output '{1}'",
                 errorText,
                 text);
 
             InitializeBuildEngine();
 
+            var invokedPath = string.Empty;
+            var invokedArgs = new List<string>();
+            var invokedWorkingDirectory = string.Empty;
+            Action<StringDictionary> environmentVariableBuilder = null;
+            var invoker = new Mock<IApplicationInvoker>();
+            {
+                invoker.Setup(
+                    i => i.Invoke(
+                        It.IsAny<string>(),
+                        It.IsAny<IEnumerable<string>>(),
+                        It.IsAny<string>(),
+                        It.IsAny<Action<StringDictionary>>(),
+                        It.IsAny<DataReceivedEventHandler>(),
+                        It.IsAny<DataReceivedEventHandler>(),
+                        It.IsAny<bool>()))
+                    .Callback<string, IEnumerable<string>, string, Action<StringDictionary>, DataReceivedEventHandler, DataReceivedEventHandler, bool>(
+                        (path, args, dir, e, o, err, f) =>
+                        {
+                            invokedPath = path;
+                            invokedArgs.AddRange(args);
+                            invokedWorkingDirectory = dir;
+                            environmentVariableBuilder = e;
+                        })
+                    .Returns(-1);
+            }
+
             var task = new InvokePowershellCommand();
             task.BuildEngine = BuildEngine.Object;
             task.IgnoreErrors = false;
-
             task.Command = command;
+
             var result = task.Execute();
 
             Assert.IsFalse(result);
