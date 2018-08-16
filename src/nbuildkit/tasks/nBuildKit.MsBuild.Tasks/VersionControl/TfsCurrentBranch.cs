bfe570572cdb81a5c900e88779f446f44dc24f8d
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-08-17 11:49:04 +1200

    TFS doesn't always know there is a workspace so we have to force it to refresh the cache(?)
    references #335

--- src/nbuildkit/tasks/nBuildKit.MsBuild.Tasks/VersionControl/TfsCurrentBranch.cs
+++ src/nbuildkit/tasks/nBuildKit.MsBuild.Tasks/VersionControl/TfsCurrentBranch.cs
@@ -6,6 +6,7 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Diagnostics;
 using System.Globalization;
 using Microsoft.Build.Framework;
 using NBuildKit.MsBuild.Tasks.Core;
@@ -48,19 +49,39 @@ namespace NBuildKit.MsBuild.Tasks.VersionControl
             set;
         }
 
+        /// <summary>
+        /// Gets or sets the name of the user which was used to create the workspace.
+        /// </summary>
+        public string UserName
+        {
+            get;
+            set;
+        }
+
         /// <inheritdoc/>
         public override bool Execute()
         {
-            var output = GetTfOutput(new[] { "workfold" });
-
-            // Expecting the output to be something like:
-            // Workspace : MyMachine (Darth Vader)
-            // Collection: http://tfs:8080/tfs/DeathStar
-            // $/: C:\vcs\tfs\deathstar
-            //
-            // So the item we're looking for is the third line
-            var lines = output.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-            if (lines.Length < 3)
+            // For some reason TFS is being silly and it won't actually get any information about the workspace
+            // even if we're in it. So we force it to get information about all the workspaces so that it refreshes
+            // the machine wide caches
+            DataReceivedEventHandler standardOutputHandler = (s, e) =>
+                {
+                    if (!string.IsNullOrWhiteSpace(e.Data))
+                    {
+                        Log.LogMessage(MessageImportance.Low, e.Data);
+                    }
+                };
+            var exitCode = InvokeTf(
+                new[]
+                {
+                    "workspaces",
+                    string.Format(
+                        CultureInfo.InvariantCulture,
+                        "/owner:{0}",
+                        UserName ?? Environment.UserName)
+                },
+                standardOutputHandler);
+            if (exitCode != 0)
             {
                 Log.LogError(
                     string.Empty,
@@ -75,7 +96,33 @@ namespace NBuildKit.MsBuild.Tasks.VersionControl
                 return false;
             }
 
-            var line = lines[2];
+            var output = GetTfOutput(new[] { "workfold" });
+
+            // Expecting the output to be something like:
+            //
+            // ==============================================
+            // Workspace : MyMachine (Darth Vader)
+            // Collection: http://tfs:8080/tfs/DeathStar
+            // $/: C:\vcs\tfs\deathstar
+            //
+            // So the item we're looking for is the fourth line
+            var lines = output.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            if (lines.Length < 4)
+            {
+                Log.LogError(
+                    string.Empty,
+                    ErrorCodeById(ErrorIdNoWorkspace),
+                    ErrorIdNoWorkspace,
+                    string.Empty,
+                    0,
+                    0,
+                    0,
+                    0,
+                    "No workspace could be found to gather the TFS branch from.");
+                return false;
+            }
+
+            var line = lines[3];
             var sections = line.Split(new[] { ": " }, StringSplitOptions.RemoveEmptyEntries);
             if (sections.Length < 2)
             {
--- src/nbuildkit/tasks/nBuildKit.MsBuild.Tasks/VersionControl/TfsCurrentChangeset.cs
+++ src/nbuildkit/tasks/nBuildKit.MsBuild.Tasks/VersionControl/TfsCurrentChangeset.cs
@@ -6,6 +6,8 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Diagnostics;
+using System.Globalization;
 using Microsoft.Build.Framework;
 using NBuildKit.MsBuild.Tasks.Core;
 
@@ -46,9 +48,53 @@ namespace NBuildKit.MsBuild.Tasks.VersionControl
             set;
         }
 
+        /// <summary>
+        /// Gets or sets the name of the user which was used to create the workspace.
+        /// </summary>
+        public string UserName
+        {
+            get;
+            set;
+        }
+
         /// <inheritdoc/>
         public override bool Execute()
         {
+            // For some reason TFS is being silly and it won't actually get any information about the workspace
+            // even if we're in it. So we force it to get information about all the workspaces so that it refreshes
+            // the machine wide caches
+            DataReceivedEventHandler standardOutputHandler = (s, e) =>
+            {
+                if (!string.IsNullOrWhiteSpace(e.Data))
+                {
+                    Log.LogMessage(MessageImportance.Low, e.Data);
+                }
+            };
+            var exitCode = InvokeTf(
+                new[]
+                {
+                    "workspaces",
+                    string.Format(
+                        CultureInfo.InvariantCulture,
+                        "/owner:{0}",
+                        UserName ?? Environment.UserName)
+                },
+                standardOutputHandler);
+            if (exitCode != 0)
+            {
+                Log.LogError(
+                    string.Empty,
+                    ErrorCodeById(ErrorIdNoWorkspace),
+                    ErrorIdNoWorkspace,
+                    string.Empty,
+                    0,
+                    0,
+                    0,
+                    0,
+                    "No workspace could be found to gather the TFS branch from.");
+                return false;
+            }
+
             var output = GetTfOutput(new[] { "history . /recursive /noprompt /stopafter:1 /version:W" });
 
             // Expecting the output to be something like:
