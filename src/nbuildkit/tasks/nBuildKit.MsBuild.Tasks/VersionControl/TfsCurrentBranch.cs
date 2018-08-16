bb87e9542b45c3c292aa1ccd21df740e150a054b
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-08-16 12:21:27 +1200

    Adding tasks to get the current changeset number and the server path from TFS
    references #335

---
+++ src/nbuildkit/tasks/nBuildKit.MsBuild.Tasks/VersionControl/TfsCurrentBranch.cs
@@ -0,0 +1,108 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="nBuildKit">
+// Copyright (c) nBuildKit. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Globalization;
+using Microsoft.Build.Framework;
+using NBuildKit.MsBuild.Tasks.Core;
+using NBuildKit.MsBuild.Tasks.Core.FileSystem;
+
+namespace NBuildKit.MsBuild.Tasks.VersionControl
+{
+    /// <summary>
+    /// Defines a <see cref="ITask"/> that performs a TFS query to find the current branch of the workspace.
+    /// </summary>
+    public sealed class TfsCurrentBranch : TfCommandLineToolTask
+    {
+        private const string ErrorIdNoMapping = "NBuildKit.Vcs.Tfs.NoMapping";
+        private const string ErrorIdNoWorkspace = "NBuildKit.Vcs.Tfs.NoWorkspace";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TfsCurrentBranch"/> class.
+        /// </summary>
+        public TfsCurrentBranch()
+            : this(null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TfsCurrentBranch"/> class.
+        /// </summary>
+        /// <param name="invoker">The object which handles the invocation of the command line applications.</param>
+        public TfsCurrentBranch(IApplicationInvoker invoker)
+            : base(invoker)
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the current branch.
+        /// </summary>
+        [Output]
+        public string CurrentBranch
+        {
+            get;
+            set;
+        }
+
+        /// <inheritdoc/>
+        public override bool Execute()
+        {
+            var output = GetTfOutput(new[] { "workfold" });
+
+            // Expecting the output to be something like:
+            // Workspace : MyMachine (Darth Vader)
+            // Collection: http://tfs:8080/tfs/DeathStar
+            // $/: C:\vcs\tfs\deathstar
+            //
+            // So the item we're looking for is the third line
+            var lines = output.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            if (lines.Length < 3)
+            {
+                Log.LogError(
+                    string.Empty,
+                    ErrorCodeById(ErrorIdNoWorkspace),
+                    ErrorIdNoWorkspace,
+                    string.Empty,
+                    0,
+                    0,
+                    0,
+                    0,
+                    "No workspace could be found to gather the TFS branch from.");
+                return false;
+            }
+
+            var line = lines[2];
+            var sections = line.Split(new[] { ": " }, StringSplitOptions.RemoveEmptyEntries);
+            if (sections.Length < 2)
+            {
+                Log.LogError(
+                    string.Empty,
+                    ErrorCodeById(ErrorIdNoMapping),
+                    ErrorIdNoMapping,
+                    string.Empty,
+                    0,
+                    0,
+                    0,
+                    0,
+                    "No change set number could be found. The tf.exe output was '{0}'.",
+                    output);
+                return false;
+            }
+
+            var currentDirectory = GetAbsolutePath(Workspace);
+            var relativePath = PathUtilities.GetDirectoryPathRelativeToDirectory(currentDirectory, sections[1].Trim());
+
+            CurrentBranch = string.Format(
+                CultureInfo.InvariantCulture,
+                "{0}/{1}",
+                sections[0].Trim().TrimEnd('/'),
+                relativePath.TrimStart('\\').Replace('\\', '/'));
+
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
---
+++ src/nbuildkit/tasks/nBuildKit.MsBuild.Tasks/VersionControl/TfsCurrentChangeset.cs
@@ -0,0 +1,99 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="nBuildKit">
+// Copyright (c) nBuildKit. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENCE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using Microsoft.Build.Framework;
+using NBuildKit.MsBuild.Tasks.Core;
+
+namespace NBuildKit.MsBuild.Tasks.VersionControl
+{
+    /// <summary>
+    /// Defines a <see cref="ITask"/> that performs a TFS query to find the current changeset of the workspace.
+    /// </summary>
+    public sealed class TfsCurrentChangeSet : TfCommandLineToolTask
+    {
+        private const string ErrorIdNoChangesetNumber = "NBuildKit.Vcs.Tfs.NoChangeSet";
+        private const string ErrorIdNoWorkspace = "NBuildKit.Vcs.Tfs.NoWorkspace";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TfsCurrentChangeSet"/> class.
+        /// </summary>
+        public TfsCurrentChangeSet()
+            : this(null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TfsCurrentChangeSet"/> class.
+        /// </summary>
+        /// <param name="invoker">The object which handles the invocation of the command line applications.</param>
+        public TfsCurrentChangeSet(IApplicationInvoker invoker)
+            : base(invoker)
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the current change set.
+        /// </summary>
+        [Output]
+        public string CurrentChangeSet
+        {
+            get;
+            set;
+        }
+
+        /// <inheritdoc/>
+        public override bool Execute()
+        {
+            var output = GetTfOutput(new[] { "history . /recursive /noprompt /stopafter:1 /version:W" });
+
+            // Expecting the output to be something like:
+            // Changeset User              Date       Comment
+            // --------- ----------------- ---------- ----------------------------------------
+            // 123456    Darth Vader 27/07/2199 Update death star security settings
+            //
+            // So the item we're looking for is the 123456 number at the start of the 3rd line
+            var lines = output.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            if (lines.Length < 3)
+            {
+                Log.LogError(
+                    string.Empty,
+                    ErrorCodeById(ErrorIdNoWorkspace),
+                    ErrorIdNoWorkspace,
+                    string.Empty,
+                    0,
+                    0,
+                    0,
+                    0,
+                    "No workspace could be found to gather the TFS change set number from.");
+                return false;
+            }
+
+            var line = lines[2];
+            var indexOfSpace = line.IndexOf(' ');
+            if (indexOfSpace < 0)
+            {
+                Log.LogError(
+                    string.Empty,
+                    ErrorCodeById(ErrorIdNoChangesetNumber),
+                    ErrorIdNoChangesetNumber,
+                    string.Empty,
+                    0,
+                    0,
+                    0,
+                    0,
+                    "No change set number could be found. The tf.exe output was '{0}'.",
+                    output);
+                return false;
+            }
+
+            CurrentChangeSet = line.Substring(0, indexOfSpace).Trim();
+
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
